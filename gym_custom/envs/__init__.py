20ff7bf1bc4c5e51819d9bf5ee1c61e3973e78bb
BaeJaeyong <qowodyd0116@gmail.com>
2021-08-12 14:19:24 +0900

    # initial commit

---
+++ gym_custom/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_custom.envs.custom_env import CustomEnv
+# points to the location where the class that inherits from gym.Env can be found
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_custom', # name of the package
+      version='0.0.1', # version of this release
+      install_requires=['gym'] # specifies the minimal list of libraries required to run the package correctly
+)
